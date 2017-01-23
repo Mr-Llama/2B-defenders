9ad9dc00effa72bc4de9ec8d5f373b989f0b83b3
Abhinav Raghavendra <mac6@129mac18.valleychristian.net>
2017-01-23 11:02:26 -0800

    Added basic JS

--- webapp.js
+++ webapp.js
@@ -1,1 +1,21 @@
 // JavaScript Document
+var wood = 0;
+
+function day() {
+	document.body.style.backgroundColor = "yellow";
+		
+}
+
+function night() {
+	document.body.style.backgroundColor = "black";
+		
+}
+
+function grabWood() {
+	wood+=1;
+}
+
+function startFire() {
+	
+}
+
