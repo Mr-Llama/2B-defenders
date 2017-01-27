ecd0d6216cbb4962db1382a7c3a379b2afe02dd7
Nicholas Le <mac6@129mac18.valleychristian.net>
2017-01-27 11:05:57 -0800

    Revert "Merge branch 'JS' into Version-1.0"
    This reverts commit f4af9d8c10ce459da9719ba9032cb7d3b6f277d9, reversing
    changes made to 4bfacd4b1cdc0d1ca888f39aa2c57b851623dd96.

--- webapp.js
+++ webapp.js
@@ -1,6 +1,12 @@
 // JavaScript Document
+
+
+
+
+//jQuery
+
 var wood = 0;
-var fireMessage=document.write("You have started a fire" + "<br>");
+
 function day() {
 	document.body.style.backgroundColor = "yellow";
 		
@@ -13,19 +19,13 @@ function night() {
 
 function grabWood() {
 	wood+=1;
-	
-	document.getElementById("d").value = wood;
-
-	
 }
 
 function startFire() {
 	
-	 if(wood >= 5) {
-	 		wood-=5;
-			document.getElementById("d").value = wood;
-			document.getElementById("text").value=fireMessage;
-			
+	 if(wood >= 1) {
+	 		wood-=1;
+			alert("You have started a fire");
 			
 	 }else {
 	 	alert("Not enough wood");
@@ -33,15 +33,3 @@ function startFire() {
 }
 
 
-var house = 0;
-function buildHouse() {
-	
-	 if(wood >= 100) {
-	 		wood-=100;
-			house +=1
-			alert("You have build a house");
-			
-	 }else {
-	 	alert("Not enough wood");
-	 }
-}
