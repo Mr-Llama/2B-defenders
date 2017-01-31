d782be7dc62385cba5987762e85b0db0f03d65e5
Nicholas Le <mac6@129mac18.valleychristian.net>
2017-01-31 11:11:20 -0800

    added trading function

--- webapp.js
+++ webapp.js
@@ -67,3 +67,14 @@ function makeTrap() {
 	 }
 }
 
+function trade() {
+	 var tradasaurous rex = Math.random	;
+	  
+	  if(tradeasaurous rex =< 0.2) {
+			confirm("Scary guys wanna trade 10 wood for 3 fur");
+	  }
+	  if else (tradeasaurous rex =< 0.4) {
+			  confirm("Scary guys wanna trade 10 wood for 2 porkchops fur");
+	  }
+}
+
