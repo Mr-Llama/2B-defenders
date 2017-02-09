92efd70d45fdc166ad6fcc17391017bbdbc33085
Abhinav Raghavendra <mac6@129mac18.valleychristian.net>
2017-02-09 10:37:04 -0800

    add text function

--- webapp.js
+++ webapp.js
@@ -6,39 +6,17 @@ var gems = 0;
 var leather = 0;
 var diamonds = 0;
 var fur = 0;
-var text1 = "";
-var text2 = "";
-var text3 = "";
-var text4 = "";
-var text5 = "";
-var text6 = "";
-var text7 = "";
-var text8 = "";
-var text9 = "";
-var text10 = "";
+var text1;
+var amount_l;
+var amount_f;
 
 
-function c_text(text) {
-	 /*text10 = text9;
-	text9 = text8;
-	text8 = text7;
-	text7 = text6;
-	text6 = text5;
-	text5 = text4;
-	text4 = text3;
-	text3 = text2;
-	text2 = text1;
-	text1 = text;*/
-	document.getElementByID("text1").innerHTML = text;
-	document.getElementByID("text2").innerHTML = text2;
-	document.getElementByID("text3").innerHTML = text3;
-	document.getElementByID("text4").innerHTML = text4;
-	document.getElementByID("text5").innerHTML = text5;
-	document.getElementByID("text6").innerHTML = text6;
-	document.getElementByID("text7").innerHTML = text7;
-	document.getElementByID("text8").innerHTML = text8;
-	document.getElementByID("text9").innerHTML = text9;
-	document.getElementByID("text10").innerHTML = text10;
+
+function c_text(text1) {
+ 
+	document.getElementById("text1").innerHTML = text1;
+	document.getElementById("text1").value =  text1;
+	
 }
 
 
@@ -79,33 +57,42 @@ function startFire() {
 
 			
 	 }else {
-	 	alert("Not enough wood");
+	 	c_text("Not enough wood");
 	 }
 }
 
 var traps= 0;
 function makeTrap() {
-	var trapMessage="You have built a trap";
 	 if(wood >= 10) {
+		  c_text("You created A TRAP!!")
 	 		wood -= 10;
 			traps += 1;
 			document.getElementById("traps").value = "traps: " + traps;
 			document.getElementById("wood").value = "wood: " + wood;			
 	 }else {
-	 	alert("Not enough wood");
+	 	c_text("Not enough wood");
 	 }
 }
 
 function checkTraps(){
 		if (traps >= 1) {
 			for (i = 0; i < traps; i++){
-				leather += Math.round(Math.random()*10);
+				amount_l = Math.round(Math.random()*10);
+				amount_f = Math.round(Math.random() * 5);
+				leather += amount_l;
+				fur +=  amount_f;
+				traps -= 1;
+				c_text("The traps contained " + amount_l + " leather and " + amount_f + " fur." );
 				document.getElementById("leather").value =  leather; 
 				document.getElementById("leather").value = "leather: " + leather;
+				document.getElementById("traps").value = traps;
+				document.getElementById("traps").value="traps: " + traps;
+				document.getElementById("fur").value = fur;
+				document.getElementById("fur").value="fur: " + fur;
 			}
 		}
 		else {
-			alert("You have no traps, idiot...");
+			c_text("You have no traps, idiot...");
 		}
 }
 
@@ -117,7 +104,7 @@ switch(chooseNum) {
 	
 				var firstCase = confirm("Would you like to trade 20 leather for 1 gem?");
 				if(firstCase === false){
-					alert("FINE DON'T TRADE! I DONT CARE!");
+					c_text("FINE DON'T TRADE! I DONT CARE!");
 					break;
 				}
 				if(firstCase && leather >= 20) {
@@ -128,7 +115,7 @@ switch(chooseNum) {
 					document.getElementById("gems").value =  gems; 
 					document.getElementById("gems").value = "gems: " + gems;	
 					}else{
-						alert("You don't have enough leather...");
+						c_text("You don't have enough leather...");
 					}
 					break;
 	case 4:
@@ -136,7 +123,7 @@ switch(chooseNum) {
 	case 6:
 			var secondCase = confirm("Would you like to trade 100 wood for 10 fur?");
 				if(secondCase === false){
-					alert("FINE DON'T TRADE! I DONT CARE!");
+					c_text("FINE DON'T TRADE! I DONT CARE!");
 					break;
 				}
 					if(secondCase && wood >= 100) {
@@ -147,7 +134,7 @@ switch(chooseNum) {
 					document.getElementById("fur").value =  fur; 
 					document.getElementById("fur").value = "fur: " + fur;	
 					}else{
-						alert("You don't have enough wood...");
+						c_text("You don't have enough wood...");
 				}
 				break;
 	case 7:
@@ -155,7 +142,7 @@ switch(chooseNum) {
 	
 				var thirdCase = confirm("Would you like to trade 10 gems for 1 diamond?");
 				if(thirdCase === false){
-					alert("FINE DON'T TRADE! I DONT CARE!");
+					c_text("FINE DON'T TRADE! I DONT CARE!");
 					break;
 				}
 					if(thirdCase && gems >= 10) {
@@ -166,13 +153,13 @@ switch(chooseNum) {
 					document.getElementById("diamonds").value =  diamonds; 
 					document.getElementById("diamonds").value = "diamonds: " + diamonds;
 					}else {
-						alert("You don't have enough gems...");
+						c_text("You don't have enough gems...");
 				}
 				break;
 	case 10: 
 				var fourthCase = confirm("Would you like to trade 1000 wood for 1 diamond?");
 				if(fourthCase === false){
-					alert("FINE DON'T TRADE! I DONT CARE!");
+					c_text("FINE DON'T TRADE! I DONT CARE!");
 					break;
 				}
 					if(fourthCase && wood >= 1000 ) {
@@ -183,14 +170,14 @@ switch(chooseNum) {
 					document.getElementById("diamonds").value =  diamonds; 
 					document.getElementById("diamonds").value = "diamonds: " + diamonds;
 					}else{
-						alert("You don't have enough wood...");
+						c_text("You don't have enough wood...");
 				}
 				break;
 	default:
 	
 			   var fifthCase = confirm("Would you like to trade 50 leather for 5 gems?");
 			   if(fifthCase === false){
-					alert("FINE DON'T TRADE! I DONT CARE!");
+					c_text("FINE DON'T TRADE! I DONT CARE!");
 					break;
 				}
 			   	if(fifthCase && leather >= 50) {
@@ -201,7 +188,7 @@ switch(chooseNum) {
 					document.getElementById("gems").value =  gems; 
 					document.getElementById("gems").value = "gems: " + gems;	
 					}else{
-						alert("You don't have enough leather...");
+						c_text("You don't have enough leather...");
 				}
 				break;
 }
